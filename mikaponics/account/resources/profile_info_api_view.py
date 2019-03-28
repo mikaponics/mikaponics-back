e3b93742e23b15eb63838335a17a630686aaab95
bartmika <bart@mikasoftware.com>
2019-03-28 15:41:46 -0400

    Added API token being returned in the profile dashboard.

--- mikaponics/account/resources/profile_info_api_view.py
+++ mikaponics/account/resources/profile_info_api_view.py
@@ -2,11 +2,13 @@
 import django_filters
 from ipware import get_client_ip
 from oauth2_provider.contrib.rest_framework import OAuth2Authentication, TokenHasScope
+from django.conf import settings
 from django.db import transaction
 from django.http import Http404
 from django_filters import rest_framework as filters
 from django.conf.urls import url, include
 from django.utils import timezone
+from oauth2_provider.models import Application, AbstractApplication, AbstractAccessToken, AccessToken, RefreshToken
 from rest_framework import generics
 from rest_framework import authentication, viewsets, permissions, status,  parsers, renderers
 from rest_framework.response import Response
@@ -37,12 +39,17 @@ class ProfileInfoRetrieveUpdateAPIView(generics.RetrieveUpdateDestroyAPIView):
         """
         client_ip, is_routable = get_client_ip(self.request)
         self.check_object_permissions(request, request.user)  # Validate permissions.
+
+        # Fetch our application and token for the user.
+        application = Application.objects.filter(name=settings.MIKAPONICS_RESOURCE_SERVER_NAME).first()
+        access_token = AccessToken.objects.filter(user=request.user, application=application).order_by('-created').first()
+
         serializer = ProfileInfoRetrieveUpdateSerializer(request.user, context={
             'authenticated_by': request.user,
             'authenticated_from': client_ip,
             'authenticated_from_is_public': is_routable,
-            'token': None,
-            'scope': None,
+            'token': str(access_token),
+            'scope': 'read,write,introspection'
         })
         return Response(serializer.data, status=status.HTTP_200_OK)
 
@@ -53,12 +60,17 @@ class ProfileInfoRetrieveUpdateAPIView(generics.RetrieveUpdateDestroyAPIView):
         """
         client_ip, is_routable = get_client_ip(self.request)
         self.check_object_permissions(request, request.user)  # Validate permissions.
+
+        # Fetch our application and token for the user.
+        application = Application.objects.filter(name=settings.MIKAPONICS_RESOURCE_SERVER_NAME).first()
+        access_token = AccessToken.objects.filter(user=request.user, application=application).order_by('-created').first()
+
         serializer = ProfileInfoRetrieveUpdateSerializer(request.user, data=request.data, context={
             'authenticated_by': request.user,
             'authenticated_from': client_ip,
             'authenticated_from_is_public': is_routable,
-            'token': None,
-            'scope': None,
+            'token': str(access_token),
+            'scope': 'read,write,introspection'
         })
         serializer.is_valid(raise_exception=True)
         serializer.save()
