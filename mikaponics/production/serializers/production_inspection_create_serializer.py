bef36f3f636b395de8540e27136fc11c34a27922
bartmika <bart@mikasoftware.com>
2019-07-07 11:00:28 -0400

    BUGFIX: Fixed issue with create production inspection api

--- mikaponics/production/serializers/production_inspection_create_serializer.py
+++ mikaponics/production/serializers/production_inspection_create_serializer.py
@@ -23,7 +23,7 @@ class ProductionInspectionCreateSerializer(serializers.ModelSerializer):
     slug = serializers.SlugField(read_only=True,)
     state = serializers.IntegerField(read_only=True,)
     did_pass = serializers.BooleanField(required=True,)
-    failure_reason = serializers.CharField(required=False, allow_blank=True)
+    failure_reason = serializers.CharField(required=False, allow_blank=True, allow_null=True,)
     notes = serializers.CharField(required=False, allow_blank=True)
     crop_inspections = serializers.JSONField(required=True, allow_null=False)
 
